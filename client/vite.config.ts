a0825662b0e66a09b0d5de1aa958d006572397a3
mattuu0 <tomatomato170@gmail.com>
2025-11-16 20:03:35 +0900

    自動ビルド設定を追加

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/whiteboard-screenshare/"
+  base: "/whiteboard-screenshare/",
+  build: {
+    outDir: '../docs' // ビルド成果物を親ディレクトリの docs に出力
+  }
 })
